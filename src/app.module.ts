7fafff2f4ad360a36755dbc16305ea9fd59001b9
vladkov15 <vkov2001@gmail.com>
2023-04-17 19:19:13 +0300

    feat: add template

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  controllers: [],
+  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+        envFilePath: '.env'
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '14082001vlad',
+      database: 'cinema',
+      models: [],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
